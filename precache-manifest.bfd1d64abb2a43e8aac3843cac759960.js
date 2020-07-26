842efa32968696bcf7968bb01fa5cd4698d3a773
wesker <alexandr_071@bk.ru>
2020-07-26 20:40:16 +0300

    Updates

---
+++ precache-manifest.bfd1d64abb2a43e8aac3843cac759960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c266bf08f6b7a4a7876cf5f492c259f3",
+    "url": "/future/index.html"
+  },
+  {
+    "revision": "153b922d1ebf049bc9b9",
+    "url": "/future/static/css/main.40c2fa31.chunk.css"
+  },
+  {
+    "revision": "00ac191356e83ef4e897",
+    "url": "/future/static/js/2.a17a4541.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/future/static/js/2.a17a4541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153b922d1ebf049bc9b9",
+    "url": "/future/static/js/main.af6041fa.chunk.js"
+  },
+  {
+    "revision": "6c500afe0c75b9d15707",
+    "url": "/future/static/js/runtime-main.b2193a76.js"
+  }
+]);
